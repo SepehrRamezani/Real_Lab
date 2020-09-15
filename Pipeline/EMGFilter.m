98d45575cb9a7af7b89056b5d2280a66d1948f06
jmdranetz <67075449+jmdranetz@users.noreply.github.com>
2020-09-15 17:15:38 -0400

    Added a EMGFilter.m function
    The function processes individual columns of raw EMG data

---
+++ Pipeline/EMGFilter.m
@@ -0,0 +1,14 @@
+function [EMGfilt] = EMGFilter(EMGdata,hpvar,lpvar,bworder,samfreq)
+%The function inputs a column of vector of raw EMG data and filter settings
+%and passes it through a high pass filter, a rectifier, and then a low pass
+%filter
+
+%High pass filter
+[bb,aa] = butter(bworder, hpvar/(samfreq/2),'high');
+datafilt=filtfilt(bb,aa,EMGdata);
+%Rectifying EMG data
+datafilt=abs(datafilt);
+%Low pass filter
+[bb,aa] = butter(bworder, lpvar/(samfreq/2),'low');
+EMGfilt=filter(bb,aa,datafilt);
+end
