100ae81aeff21f0d0333b3f1d7728f3b4dacaff0
Sepehr Ramezani <ramezani.dan@gmail.com>
2020-12-16 00:20:31 -0500

    Adding other signal checker
    It will check other signal is recorded.

--- Pipeline/EMGScript.m
+++ Pipeline/EMGScript.m
@@ -7,7 +7,7 @@ EMG=importdata(wholefile,',');
 
 Data=EMG.data;
 HData=EMG.colheaders;
-[rg,cg]=find(strncmp(HData,'Gn',2));
+[rg,cg]=find(strncmp(HData,'Gn A',4));
 %find Knee Goniometer
 [rk,ck]=find(strncmp(HData,'Gn K',4));
 %find Hip goniometer
@@ -16,10 +16,14 @@ HData=EMG.colheaders;
 [rb,cb]=find(strncmp(HData,'Biodex',6));
 %find EMG
 [re,ce]=find(contains(HData,'EMG')&~contains(HData,'RMS'));
+er={};
+if ~(sum(rg)&sum(rk)&sum(rh)&sum(rb)&sum(re))
+er=[er; 'Data is incomplete'];
+end
 
 DStime=Data(2,ce(1)-1)-Data(1,ce(1)-1); % desired sampling time
 Namedr='placeholder';
-er={};
+
 for i=1:length(ce)
     g=EMGChecker(Data(:,ce(i)),Namedr,HData{ce(i)},DStime);
     if g==1
