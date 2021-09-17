ae4d7fddee4c704c938248bfed69237f34cdcfc2
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2021-09-17 13:52:31 -0400

    Generic function to write data

---
+++ Pipeline/makefile.m
@@ -0,0 +1,25 @@
+function makefile (folder,F_fnames,Title,Titledata,Dataheader,Datadata,Resulotion,delimiterIn)
+% Resulotion number of digit after zero
+           fid=fopen(append(folder,'\',F_fnames), "w");
+            if fid < 0
+                fprintf('\nERROR: %s could not be opened for writing...\n\n', fname);
+            return
+            end
+            [r,c]=size(Datadata);
+            ft=[];
+            for f=1:c
+                ft=[ft '%.' num2str(Resulotion) 'f' delimiterIn];
+            end
+            fprintf(fid,[char(F_fnames) Title],Titledata);
+            fprintf(fid, '\n');
+            for k=1:length(Dataheader)
+            fprintf(fid,[char(Dataheader(k)) delimiterIn]);
+            end
+            fprintf(fid, '\n');
+                for i = 1:r
+                    fprintf(fid,ft,Datadata(i,:));
+                    fprintf(fid, '\n');
+                end
+            fclose(fid);
+            fprintf('Saved %s\n', append(folder,'\',F_fnames))
+end
