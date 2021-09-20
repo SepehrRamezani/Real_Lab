052f7f43db4f55ff98c1e47d60ca6a7c33ee4af7
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2021-09-20 12:26:10 -0400

    Remove extra "\"

--- Pipeline/makefile.m
+++ Pipeline/makefile.m
@@ -1,6 +1,6 @@
 function makefile (folder,F_fnames,Title,Titledata,Dataheader,Datadata,Resulotion,delimiterIn)
 % Resulotion number of digit after zero
-           fid=fopen(append(folder,'\',F_fnames), "w");
+           fid=fopen(append(folder,F_fnames), "w");
             if fid < 0
                 fprintf('\nERROR: %s could not be opened for writing...\n\n', fname);
             return
@@ -21,5 +21,5 @@ function makefile (folder,F_fnames,Title,Titledata,Dataheader,Datadata,Resulotio
                     fprintf(fid, '\n');
                 end
             fclose(fid);
-            fprintf('Saved %s\n', append(folder,'\',F_fnames))
+            fprintf('Saved %s\n', append(folder,F_fnames))
 end
